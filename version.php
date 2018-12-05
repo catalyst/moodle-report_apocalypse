7f8ec74fbe317fdc5fa75a2ff775163e0376571f
Dan Marsden <dan@danmarsden.com>
2018-12-06 07:25:58 +1300

    bump version for plugins db release.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018051400;
+$plugin->version   = 2018120600;
 $plugin->requires  = 2015051109; // Requires Moodle 2.9.
 $plugin->component = 'report_apocalypse';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1';
+$plugin->release   = '1.2';
